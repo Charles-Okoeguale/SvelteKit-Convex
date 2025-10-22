4d30b02363cc29453fd784f67a4ec7ead7537c98
Charles <osehaeguale@gmail.com>
2025-10-22 12:23:16 +0100

    adjust fade animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
   import { PUBLIC_CONVEX_URL } from '$env/static/public';
   import Navigation from '$lib/components/Navigation.svelte';
   import { afterNavigate } from '$app/navigation';
-    import { writable } from 'svelte/store';
+  import { writable } from 'svelte/store';
 
   const routeOrder = ['/', '/customers', '/dashboard', '/rewards'];
   let animationClass = writable('');
@@ -14,21 +14,14 @@
   const convex = new ConvexClient(PUBLIC_CONVEX_URL);
   setContext('convex', convex);
 
-   afterNavigate(({ from, to }) => {
+  afterNavigate(({ from, to }) => {
     if (!from || !to) return;
 
-    const fromIndex = routeOrder.indexOf(from.url.pathname);
-    const toIndex = routeOrder.indexOf(to.url.pathname);
-
-    if (toIndex > fromIndex) {
-      animationClass.set('slide-in-right');
-    } else {
-      animationClass.set('slide-in-left');
-    }
+    animationClass.set('fade-in');
 
     setTimeout(() => {
       animationClass.set('');
-    }, 400);
+    }, 600);
 
     previousPath = from.url.pathname;
   });
@@ -36,7 +29,7 @@
   $: currentPath = $page.url.pathname;
 </script>
 
-<div class="min-h-screen bg-gray-50">
+<div class="min-h-screen bg-gray-900">
   <Navigation {currentPath} />
 
   <main class="page-transition {$animationClass}">
@@ -44,42 +37,23 @@
   </main>
 </div>
 
-
-
 <style>
   .page-transition {
-    animation-duration: 0.35s;
-    animation-timing-function: ease-out;
+    animation-duration: 0.6s;
+    animation-timing-function: ease-in-out;
     animation-fill-mode: both;
   }
 
-  .slide-in-right {
-    animation-name: slideInRight;
+  .fade-in {
+    animation-name: fadeIn;
   }
 
-  .slide-in-left {
-    animation-name: slideInLeft;
-  }
-
-  @keyframes slideInRight {
-    from {
+  @keyframes fadeIn {
+    0% {
       opacity: 0;
-      transform: translateX(30px);
     }
-    to {
+    100% {
       opacity: 1;
-      transform: translateX(0);
-    }
-  }
-
-  @keyframes slideInLeft {
-    from {
-      opacity: 0;
-      transform: translateX(-30px);
-    }
-    to {
-      opacity: 1;
-      transform: translateX(0);
     }
   }
 </style>
